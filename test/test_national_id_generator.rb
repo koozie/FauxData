3253a43806d12faefe58fe290b88fbd9e1fa6e27
Chris Stansbury <Chris@koozie.org>
2012-01-08 11:52:24 -0600

    Added two new test
    Test for SSN and test for person generator
    
    	new file:   test/test_national_id_generator.rb
    	new file:   test/test_person.rb

---
+++ test/test_national_id_generator.rb
@@ -0,0 +1,34 @@
+
+require 'rubygems'
+require 'test/unit'
+require 'pp'
+
+
+require File.join(File.dirname(__FILE__),'..','lib','faux_data','national_id_generator')
+
+class Test_SSN_Creation < Test::Unit::TestCase
+  
+    def setup
+    end
+    def teardown
+    end
+
+    def test_ssn_creation
+        nid = FauxData::NationalIdGenerator.new(:country_code => 'us')
+        id = nid.national_id
+        assert_equal(11,id.to_s.size,"SSN lenth should equal 11 with two (2) hypens and nine (9) digits: ssn [#{id}]")
+        assert_equal(9,id.to_s.delete("-").size,"SSN lenth should have length of 9 chars")
+        assert(id.to_s.delete("-") =~ /^\d{9}$/ ,"SSN lenth should equal nine (9) digits")
+    end
+
+    def test_ssn_tracking
+        nid = FauxData::NationalIdGenerator.new(:country_code => 'us')
+        ids = Array.new
+        (1..100).each do |i|
+            id = nid.national_id
+            ids << id
+        end
+        assert_equal(ids.size, nid.national_ids.size, "Both arrays should contain 100 elements ")
+        assert(ids == nid.national_ids, "Both arrays should contain the same 100 elements ")
+    end
+end
---
+++ test/test_person.rb
@@ -0,0 +1,39 @@
+
+require 'rubygems'
+require 'test/unit'
+require 'pp'
+require 'awesome_print'
+require 'date'
+
+require File.join(File.dirname(__FILE__),'..','lib','faux_data','person_generator')
+
+class Test_Person_Creation < Test::Unit::TestCase
+  
+    def setup
+    end
+    def teardown
+    end
+
+    def test_person_creation
+        required_keys = %w[ssn surname first_name middle_name street city state zip date_of_birth].map {|i| i.downcase.to_sym}
+        pg = FauxData::PersonGenerator.new
+        person = pg.person
+        assert(person.is_a?(Hash), "Person should be a hash")
+        required_keys.each do |k|
+            assert(person.key?(k), "Should have this key: #{k}")
+        end
+        assert(person[:date_of_birth].is_a?(Date), "Date of Birth should be a date object")
+
+    end
+
+    def test_ssn_tracking
+        pg = FauxData::PersonGenerator.new
+        persons = Array.new
+        (1..100).each do |i|
+            person = pg.person
+            persons << person
+        end
+        assert_equal(100 , persons.size , "Both arrays should contain 100 elements ")
+        ap persons
+    end
+end
