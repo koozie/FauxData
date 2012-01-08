3703ff966b78fbd16cd95d26c34210711715aa8a
Chris Stansbury <Chris@koozie.org>
2012-01-08 11:56:51 -0600

    Added an Example for publishing data to CSV

---
+++ examples/hundred_persons_to_csv.rb
@@ -0,0 +1,16 @@
+
+
+require 'rubygems'
+require 'csv'
+require File.join(File.dirname(__FILE__),'..','lib','faux_data','person_generator')
+
+
+col_headers =  %w[ssn surname first_name middle_name street city state zip date_of_birth sex].map {|i| i.downcase.to_sym}
+pg = FauxData::PersonGenerator.new
+
+puts CSV.generate_line(col_headers)
+(1..100).each do |i|
+    person = pg.person
+    puts CSV.generate_line(person.values_at(* col_headers))
+end
+
